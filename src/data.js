4b9e1282cace4aff32af06eceac6c9d4f7b6f1a8
Nyxwildflower <130186258+Nyxwildflower@users.noreply.github.com>
2024-08-17 12:36:45 -0500

    Added data for various components.

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const work = [
+  {
+    title: "API Weather Information",
+    tech: "React on CodeSandbox",
+    description:
+      "This project focuses on implementing an API into a React project, and my own interest in the glass polymorphism look.",
+    image: "weather_project.gif",
+    alt: "Scrolling gif of weather information for Canada in boxes with background effects that blur the cloud background image.",
+    link: "https://codesandbox.io/p/sandbox/challenge-5-madison-sobering-g5xn44",
+  },
+  {
+    title: "To-do List",
+    tech: "React on CodeSandbox",
+    description:
+      "This to-do list was used to learn about React states, and how they can be used to alter data without refreshing.",
+    image: "to_do_challenge.gif",
+    alt: "Gif of someone entering and then clearing a task from a to-do list.",
+    link: "https://codesandbox.io/p/sandbox/react-to-do-list-1hgjyl",
+  },
+];
+
+export const skills = {
+  languages: [
+    "HTML5",
+    "CSS",
+    "JavaScript",
+    "Ruby",
+    "PHP",
+    "Python",
+    "Typescript",
+    "Java",
+    "SQL",
+  ],
+  frameworks: ["Bootstrap", "Bulma", "Flask", "React", "Ruby on Rails"],
+  other: ["VS Code", "Docker Desktop", "Pycharm", "SQL Manager"],
+};
+
+export const resources = [
+  {
+    title: "LinkedIn",
+    description: "Click here to see the rest of my resume.",
+    image: "linked-in-logo.png",
+    alt: "Square blue LinkedIn icon with the word 'in' in the middle.",
+    link: "https://www.linkedin.com/in/madison-sobering-2684132b0/",
+  },
+  {
+    title: "GitHub",
+    description: "Get a look at some of my more in-depth projects.",
+    image: "github-mark-white.svg",
+    alt: "White circle with the Github cat logo in the middle.",
+    link: "https://github.com/Nyxwildflower",
+  },
+];
+
+export const setup = {
+  vsCode: {
+    settings: "vs_code_settings.png",
+    extensions: [
+      "Auto Close Tag",
+      "Docker",
+      "endwise",
+      "erb",
+      "Github Codespaces",
+      "html tag wrapper",
+      "Live Preview",
+      "Prettier",
+      "Rainbow CSV",
+      "SQLite",
+    ],
+  },
+  terminal: {
+    windows: "Powershell or Command Line",
+    linux: "WSL",
+  },
+  preferredFont: "Consolas, 'Courier New', monospace",
+};
