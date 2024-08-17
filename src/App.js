fac7c1ce1f7ee9c9a96c4d1b152040c063924e63
Nyxwildflower <130186258+Nyxwildflower@users.noreply.github.com>
2024-08-17 12:34:28 -0500

    Added components and imports on app.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
+import About from "./components/About";
+import Resources from "./components/Resources";
+import Navbar from "./components/Navbar";
+import Work from "./components/Work";
+import Skills from "./components/Skills";
+import DevSetup from "./components/DevSetup";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Work />
+      <Skills />
+      <DevSetup />
+      <Resources />
+    </main>
   );
 }
 
